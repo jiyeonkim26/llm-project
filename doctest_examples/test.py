edce7eaadb9f82775411f2f8fe5a27f40b8ee18b
Jiyeon Kim <jkim26@students.claremontmckenna.edu>
2026-04-22 13:39:55 -0700

    [docchat] add hello

--- doctest_examples/test.py
+++ doctest_examples/test.py
@@ -1,13 +1,1 @@
-import json
-
-
-def calculate(expression):
-    '''
-    This tool evaluates a mathematical expression.
-
-    >>> calculate("3 + 2")
-    '{"result": 5}'
-
-    '''
-    result = eval(expression)  # Use safe evaluation in production
-    return json.dumps({"result": result})
+hello
