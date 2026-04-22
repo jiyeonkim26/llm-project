05de5cb3d5a9025077743de91ed5ec336c27fb22
Jiyeon Kim <jkim26@students.claremontmckenna.edu>
2026-04-21 21:19:16 -0700

    update to pass doctest

--- tools/doctests.py
+++ tools/doctests.py
@@ -4,6 +4,8 @@ doctests.py defines the function that allows the doctests tool to be called and 
 import doctest
 import importlib.util
 import os
+import io
+from contextlib import redirect_stdout
 
 
 def doctests(path):
@@ -14,30 +16,29 @@ def doctests(path):
     True
 
     >>> doctests("doctest_examples/example.txt")
-    Skipping doctest_examples/example.txt: Not a .py file
+    'Skipping doctest_examples/example.txt: Not a .py file'
 
     >>> doctests("doctest_examples")
-    Skipping doctest_examples: Not a .py file
+    'Skipping doctest_examples: Not a .py file'
 
     >>> doctests("doctest_examples/does_not_exist.txt")
-    Skipping doctest_examples/does_not_exist.txt: Not a .py file
+    'Skipping doctest_examples/does_not_exist.txt: Not a .py file'
 
     >>> doctests("doctest_examples/example_utf16.txt")
-    Skipping doctest_examples/example_utf16.txt: Not a .py file
+    'Skipping doctest_examples/example_utf16.txt: Not a .py file'
     '''
     if path.endswith(".py") and os.path.isfile(path):
-        print(f"--- Running doctests for: {path} ---")
-
-        # Dynamically load the module from the path
         module_name = os.path.splitext(os.path.basename(path))[0]
         spec = importlib.util.spec_from_file_location(module_name, path)
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
 
-        # Run doctest on the imported module
-        doctest.testmod(module, verbose=True)
-    else:
-        print(f"Skipping {path}: Not a .py file")
+        output = io.StringIO()
+        with redirect_stdout(output):
+            doctest.testmod(module, verbose=True)
+        return output.getvalue()
+
+    return f"Skipping {path}: Not a .py file"
 
 
 tool_schema = {
@@ -48,7 +49,7 @@ tool_schema = {
         "parameters": {
             "type": "object",
             "properties": {
-                "filename": {
+                "path": {
                     "type": "string",
                     "description": "The path and name of the file to test"
                 }
